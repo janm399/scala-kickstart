0fd9b6f058440d964be6817b976313ba402f851a
Jan Machacek <janmachacek@Jans-MacBook-Pro.local>
2011-12-06 18:52:28 +0000

    Type systems

--- src/main/scala/org/cakesolutions/scala/intro/TypeSystems3.scala
+++ src/main/scala/org/cakesolutions/scala/intro/TypeSystems3.scala
@@ -1,31 +1,45 @@
 package org.cakesolutions.scala.intro
 
-import collection.immutable.Stack
-import org.cakesolutions.scala.intro.TypeSystems3.Cat
-
-
 /**
  * @author janmachacek
  */
 
 object TypeSystems3 {
 
-  abstract class Animal
-  class Cat extends Animal
-  class Dog extends Animal
+  abstract class Animal {
+    type SuitableFood <: Food
+  }
+  class Cat extends Animal {
+    type SuitableFood = CatFood
+  }
+  class SpeakingCat extends Cat
   
-  class Stack[+T](items: Seq[T]) {
+  abstract class Food
+  class CatFood extends Food
+  class LuxuryCatFood extends CatFood
+  class DogFood extends Food
+  
+  class Zoo[+A](animals: Seq[A]) {
     def this() = this(Nil)
-    def push[B >: T](item: B) = new Stack[B](item +: items)
+    def add[B >: A](animal: B) = new Zoo[B](animal +: animals)
   }
   
-  class S[-T]
+  class Feeder[A <: Animal](animal: A) {
+    def feed[F <: A#SuitableFood](food: F) =
+      "Fed " + food + " to " + animal
+  }
   
   def main(args: Array[String]) {
-    val animals = new Stack().push(new Cat)
-    
-    val s1: Stack[Animal] = animals
-    val s2: Stack[Any] = animals
+    val cats = new Zoo().add(new Cat)
+
+    val s1: Zoo[Cat] = cats
+    // val s2: Zoo[SpeakingCat] = cats
+    val s3: Zoo[Animal] = cats
+    val s4: Zoo[Any] = cats
+
+    new Feeder(new Cat).feed(new CatFood)
+    new Feeder(new Cat).feed(new LuxuryCatFood)
+    new Feeder(new SpeakingCat).feed(new DogFood)
   }
   
 }
