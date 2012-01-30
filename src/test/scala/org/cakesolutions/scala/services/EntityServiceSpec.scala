fc895281dbd4fb72e28df68478e919a282a40132
Jan Machacek <janmachacek@janmachacek.local>
2012-01-30 14:52:32 +0000

    Better tests

--- src/test/scala/org/cakesolutions/scala/services/EntityServiceSpec.scala
+++ src/test/scala/org/cakesolutions/scala/services/EntityServiceSpec.scala
@@ -6,22 +6,32 @@ import org.specs2.spring.{HibernateDataAccess, BeanTables, Specification}
 import org.springframework.test.context.ContextConfiguration
 import org.cakesolutions.scala.domain.{Page, User}
 
-
 /**
+ * -javaagent:/Users/janmachacek/.m2/repository/org/springframework/spring-instrument/3.1.0.RELEASE/spring-instrument-3.1.0.RELEASE.jar
+ *
  * @author janmachacek
  */
 @ContextConfiguration(Array("classpath*:/META-INF/spring/module-context.xml"))
 class EntityServiceSpec extends Specification with BeanTables with HibernateDataAccess {
   @Autowired implicit var sessionFactory: SessionFactory = _
   @Autowired var entityService: EntityService = _
-  implicit var page = Page(0, 100)
-  
+
   "find locates the inserted objects" in {
     "username" | "firstName" | "lastName" |
      "janm"   !! "Jan"       ! "Machacek" |
      "marco"  !! "Marc"      ! "Owen"     |> insert[User]
     
-    entityService.find[User].size() must_==(2)
+    entityService.get[User](1L) must_== (new User() {username = "janm"; firstName = "Jan"; lastName = "Machacek"})
+    entityService.get[User](2L) must_== (new User() {username = "marco"; firstName = "Marc"; lastName = "Owen"})
   }
 
+  "loads, saves and deletes" in {
+    val user = new User()
+    user.username = "janm"
+    user.firstName = "foo"
+    user.lastName = "bar"
+
+    entityService.save(user)
+    entityService.get[User](user.getId()) must_== (user)
+  }
 }
